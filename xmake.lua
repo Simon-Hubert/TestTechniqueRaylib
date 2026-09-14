7518afcd82b7374a101e6d3313ffa7c940186d22
Simon <s.hubert.mailpro@gmail.com>
2026-09-14 17:36:59 +0200

    [Add] Home Setup
    Setup pour que ça marche aussi chez moi sur vscode

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,13 @@
 add_rules("mode.debug", "mode.release")
 
 add_requires("raylib")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+
+add_includedirs("include")
+
 
 target("TestTech")
     set_kind("binary")
+    add_headerfiles("include/**.h")
     add_files("src/*.cpp")
     add_packages("raylib")
